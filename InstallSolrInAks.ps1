25eedabef4497d479db438ebe9d95e0ab89e1469
Tona Diaz <tdiaz@healthwise.org>
2021-02-20 08:29:23 -0700

    Added artifacts to use persistence storage from an Azure File Share

--- InstallSolrInAks.ps1
+++ InstallSolrInAks.ps1
@@ -1,58 +1,46 @@
+$mainSubscription = '065b0ab4-5905-4ce8-bada-275c71fe7696'
+$resourceGroup = 'SolrExGroup'
+$clusterName = 'SolrCluster'
+
 # Login to Azure
 az login
+az account set --subscription $mainSubscription
 
-# Create the resource group for the cluster
-az group create --name ClusterResourceGroup --location westus
+# Create resource group
+az group create --name $resourceGroup --location westus
 
-# Create a managed Kubernetes Cluster in AKS with 8 Standard DS1 VMs.
-az aks create --resource-group ClusterResourceGroup --name KubeCluster --node-count 8 --node-vm-size Standard_DS1_v2 -- enable-addons monitoring --generate-ssh-keys
+# Create file share
+$storageAccountName = 'hwsolrstorage'
+$fileShareName = 'solrstorage'
+az storage account create -n $storageAccountName -g $resourceGroup -l westus --sku Standard_LRS
+$storageAccountConnectionString = az storage account show-connection-string -n $storageAccountName -g $resourceGroup -o tsv
+az storage share create -n $fileShareName --connection-string $storageAccountConnectionString
+$storageAccountSecretKey = az storage account keys list --resource-group $resourceGroup --account-name $storageAccountName --query "[0].value" -o tsv
+
+kubectl create secret generic solr-storage-volume-key --from-literal=azurestorageaccountname=$storageAccountName --from-literal=azurestorageaccountkey=$storageAccountSecretKey
+
+# Create cluster
+az aks create --resource-group $resourceGroup --name $clusterName --node-vm-size Standard_B2ms --generate-ssh-keys --node-count 2 --enable-managed-identity
+
+# $agentPoolProfiles = az aks show --resource-group $resourceGroup --name $clusterName --query agentPoolProfiles | ConvertFrom-Json
+# $nodepoolName = $agentPoolProfiles.name
+# az aks scale --resource-group $resourceGroup --name $clusterName --node-count 3 --nodepool-name $nodepoolName
 
 # Install Kubernetes CLI (kubectl)
 az aks install-cli
 
 # Create local configuration file to talk to the AKS Cluster
-az aks get-credentials --resource-group ClusterResourceGroup --name KubeCluster
+az aks get-credentials --resource-group $resourceGroup --name $clusterName
 
-# Create a Kubernetes Service account with cluster admin privileges (aparently, not recommended for production)
-# kubectl create serviceaccount --namespace kube-system tiller
-# kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
-
-# To install helm using Chocolatey
-# choco install kubernetes-helm 
-
-# Add the incubator repository to Helm sources.
-# helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com/
-
-# Initialize Helm with the service account and install Tiller in the AKS Cluster
-# helm init --service-account tiller
-
-# Fetch the Solr charts
-helm fetch incubator/solr
-
-# Unzip-Tgz.ps1 & ExpandTar.ps1 must be in the path
-.\Unzip-Tgz.ps1 .\solr-1.2.0.tgz
-
-# Actually decompress Helm Solr chart
-.\Expand-Tar.ps1 .\solr-1.2.0.tar
-
-# Create a Solr YAML file using the Helm template
-helm template .\solr-1.2.0\solr\ --set image.tag=8.2.0 --set replicaCount=6 --set volumeClaimTemplates.storageClassName=managed-premium  --name solr > ./solr.yaml
-
-# Apply the Helm generated template to the Cluster using kubectl
-# This will install by default a 3-node Solr Cluster and a 3-node Zookeper coordinator.
-kubectl apply -f .\solr.yaml
-
-# Fetch the nginx ingress chart and decompress.
-helm fetch stable/nginx-ingress
-.\Unzip-Tgz.ps1 .\nginx-ingress-1.19.0.tgz
-.\Expand-Tar.ps1 .\nginx-ingress-1.19.0.tar
-
-# Create the YAML for the nginx-ingress artifacts
-helm template .\nginx-ingress-1.19.0\nginx-ingress --name nginx-ingress --set controller.replicaCount=2 --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux > nginx-ingress.yaml
-kubectl apply -f .\nginx-ingress.yaml
+# Configure Istio sidecar injector
+kubectl label namespace default istio-injection=enabled
 
 # Apply the predefined Ingress controller rule to the cluster
-kubectl apply -f .\solr-ingress.yaml
+kubectl apply -f .\zookeeper.yaml
+kubectl apply -f .\solr.yaml
+
+# $solrPod = kubectl get pod -l app=solr -o jsonpath='{.items[0].metadata.name}'
+# kubectl exec -i -t $solrPod -- /bin/bash
 
 # Figure out the public IP of the Solr Ingress controller and launch it in a browser
 $publicIp = kubectl get service --output=json -o jsonpath='{.items[*].status.loadBalancer.ingress[*].ip}'
