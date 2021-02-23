86e0fca697b8ccdcec69fb5b7ab55ddb7d4ea504
Tona Diaz <tdiaz@healthwise.org>
2021-02-23 14:45:36 -0700

    Updated Solr. Modified the infrastructure script to fix the order of execution.

--- InstallSolrInAks.ps1
+++ InstallSolrInAks.ps1
@@ -1,6 +1,6 @@
 $mainSubscription = '065b0ab4-5905-4ce8-bada-275c71fe7696'
-$resourceGroup = 'SolrExGroup'
-$clusterName = 'SolrCluster'
+$resourceGroup = 'MySolrGroup'
+$clusterName = 'MySolrCluster'
 
 # Login to Azure
 az login
@@ -8,16 +8,6 @@ az account set --subscription $mainSubscription
 
 # Create resource group
 az group create --name $resourceGroup --location westus
-
-# Create file share
-$storageAccountName = 'hwsolrstorage'
-$fileShareName = 'solrstorage'
-az storage account create -n $storageAccountName -g $resourceGroup -l westus --sku Standard_LRS
-$storageAccountConnectionString = az storage account show-connection-string -n $storageAccountName -g $resourceGroup -o tsv
-az storage share create -n $fileShareName --connection-string $storageAccountConnectionString
-$storageAccountSecretKey = az storage account keys list --resource-group $resourceGroup --account-name $storageAccountName --query "[0].value" -o tsv
-
-kubectl create secret generic solr-storage-volume-key --from-literal=azurestorageaccountname=$storageAccountName --from-literal=azurestorageaccountkey=$storageAccountSecretKey
 
 # Create cluster
 az aks create --resource-group $resourceGroup --name $clusterName --node-vm-size Standard_B2ms --generate-ssh-keys --node-count 2 --enable-managed-identity
@@ -27,10 +17,23 @@ az aks create --resource-group $resourceGroup --name $clusterName --node-vm-size
 # az aks scale --resource-group $resourceGroup --name $clusterName --node-count 3 --nodepool-name $nodepoolName
 
 # Install Kubernetes CLI (kubectl)
-az aks install-cli
+# az aks install-cli
 
 # Create local configuration file to talk to the AKS Cluster
 az aks get-credentials --resource-group $resourceGroup --name $clusterName
+
+# Create file share
+$storageAccountName = 'hwsolrstorage1'
+$fileShareName = 'solrstorage'
+az storage account create -n $storageAccountName -g $resourceGroup -l westus --sku Standard_LRS
+$storageAccountConnectionString = az storage account show-connection-string -n $storageAccountName -g $resourceGroup -o tsv
+az storage share create -n $fileShareName --connection-string $storageAccountConnectionString
+$storageAccountSecretKey = az storage account keys list --resource-group $resourceGroup --account-name $storageAccountName --query "[0].value" -o tsv
+
+kubectl create secret generic solr-storage-volume-key --from-literal=azurestorageaccountname=$storageAccountName --from-literal=azurestorageaccountkey=$storageAccountSecretKey
+
+# Install Istio
+istioctl install
 
 # Configure Istio sidecar injector
 kubectl label namespace default istio-injection=enabled
@@ -46,3 +49,9 @@ kubectl apply -f .\solr.yaml
 $publicIp = kubectl get service --output=json -o jsonpath='{.items[*].status.loadBalancer.ingress[*].ip}'
 $solrAddress = 'http://' + $publicIp + '/solr/#/~cloud'
 start $solrAddress
+
+# Restart pods in a deployment 
+kubectl rollout restart StatefulSet solr
+
+# From any solr node
+./server/scripts/cloud-scripts/zkcli.sh -zkhost solr-zookeeper-0.solr-zookeeper-headless.default.svc.cluster.local:2181 -cmd clusterprop -name legacyCloud -val true
